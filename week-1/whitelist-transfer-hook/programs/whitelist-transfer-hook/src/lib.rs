1bbdd1a50916db34c68123c45618f711eae74179
senzenn <ascenzen11@gmail.com>
2025-10-08 07:10:19 +0530

    week one added

---
+++ week-1/whitelist-transfer-hook/programs/whitelist-transfer-hook/src/lib.rs
@@ -0,0 +1,63 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)]
+
+use anchor_lang::prelude::*;
+
+mod instructions;
+mod state;
+
+use instructions::*;
+
+use spl_discriminator::SplDiscriminate;
+use spl_transfer_hook_interface::{
+    instruction::{
+        ExecuteInstruction, 
+        InitializeExtraAccountMetaListInstruction
+    },
+};
+use spl_tlv_account_resolution::state::ExtraAccountMetaList;
+
+declare_id!("DhzyDgCmmQzVC4vEcj2zRGUyN8Mt5JynfdGLKkBcRGaX");
+
+#[program]
+pub mod whitelist_transfer_hook {
+    use super::*;
+
+    pub fn initialize_whitelist(ctx: Context<InitializeWhitelist>) -> Result<()> {
+        ctx.accounts.initialize_whitelist(ctx.bumps)
+    }
+
+    pub fn add_to_whitelist(ctx: Context<WhitelistOperations>, user: Pubkey) -> Result<()> {
+        ctx.accounts.add_to_whitelist(user)
+    }
+
+    pub fn remove_from_whitelist(ctx: Context<WhitelistOperations>, user: Pubkey) -> Result<()> {
+        ctx.accounts.remove_from_whitelist(user)
+    }
+
+    #[instruction(discriminator = InitializeExtraAccountMetaListInstruction::SPL_DISCRIMINATOR_SLICE)]
+    pub fn initialize_transfer_hook(ctx: Context<InitializeExtraAccountMetaList>) -> Result<()> {
+
+        msg!("Initializing Transfer Hook...");
+
+        // Get the extra account metas for the transfer hook
+        let extra_account_metas = InitializeExtraAccountMetaList::extra_account_metas()?;
+
+        msg!("Extra Account Metas: {:?}", extra_account_metas);
+        msg!("Extra Account Metas Length: {}", extra_account_metas.len());
+
+        // initialize ExtraAccountMetaList account with extra accounts
+        ExtraAccountMetaList::init::<ExecuteInstruction>(
+            &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
+            &extra_account_metas
+        )?;
+
+        Ok(())
+    }
+
+    #[instruction(discriminator = ExecuteInstruction::SPL_DISCRIMINATOR_SLICE)]
+    pub fn transfer_hook(ctx: Context<TransferHook>, amount: u64) -> Result<()> {
+        // Call the transfer hook logic
+        ctx.accounts.transfer_hook(amount)
+    }
+}
